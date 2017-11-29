0b972e75536b7867ae0d9575eeb259ebd3f10c5c
raphacosta27 <rapha2706@hotmail.com>
2017-11-29 01:35:35 -0200

    First pages

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Menu from './Menu.jsx';
+import './css/materialize.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Menu/>
+      </div>
+    );
+  }
+}
+
+export default App;
